44b0a29fd96154e8cdf0a2a2aedaf787af08568c
Artem Vasiliev <github@yazasnyal.dev>
2024-02-05 18:56:59 +0000

    Begin of call and eval method implementation

--- include/tarantool_connector/box/tntpp_box.h
+++ include/tarantool_connector/box/tntpp_box.h
@@ -10,11 +10,98 @@
 namespace tntpp::box
 {
 
+class GenericResult
+{
+public:
+  GenericResult(detail::IprotoFrame frame)
+      : m_frame(std::move(frame))
+  {
+    assert(m_frame.body().data() != nullptr);
+  }
+
+  /**
+   * Convert raw buffer to the specified type
+   *
+   * @tparam T result type
+   * @return parsed object
+   */
+  template<class T>
+  T as() const
+  {
+    auto object =
+        msgpack::unpack(static_cast<const char*>(m_frame.body().data()), m_frame.body().size(), 0);
+    return object->convert();
+  }
+
+  /**
+   * Convert raw buffer to the specified type
+   *
+   * @tparam T result type
+   * @param v parsed object
+   */
+  template<class T>
+  void as(T& v) const
+  {
+    auto object =
+        msgpack::unpack(static_cast<const char*>(m_frame.body().data()), m_frame.body().size(), 0);
+    object->convert(v);
+  }
+
+  /**
+   * Convert raw buffer to the specified type
+   *
+   * This function catches exceptions and forwards them as an error_code
+   *
+   * @tparam T result type
+   * @param ec error code
+   * @return parsed object
+   */
+  template<class T>
+  T as(error_code& ec) const noexcept
+  {
+    try {
+      return as<T>();
+    } catch (const std::exception& e) {
+      ec = error_code(boost::system::errc::protocol_error, boost::system::system_category());
+    }
+  }
+
+  /**
+   * Convert raw buffer to the specified type
+   *
+   * This function catches exceptions and forwards them as an error_code
+   *
+   * @tparam T result type
+   * @param v result object
+   * @param ec error code
+   */
+  template<class T>
+  void as(T& v, error_code& ec) const noexcept
+  {
+    try {
+      as<T>(v);
+    } catch (const std::exception& e) {
+      ec = error_code(boost::system::errc::protocol_error, boost::system::system_category());
+    }
+  }
+
+  /**
+   * Get raw msgpack response
+   */
+  boost::asio::const_buffer get_raw_body() const noexcept
+  {
+    return boost::asio::const_buffer(m_frame.body().data(), m_frame.body().size());
+  }
+
+private:
+  detail::IprotoFrame m_frame;
+};
+
 class Box
 {
 public:
   Box(ConnectorSptr& conn)
-      : m_conn(conn)
+  //      : m_conn(conn)
   {
   }
   Box(const Box&) = delete;
@@ -29,9 +116,95 @@ public:
   //   insert
   //   commit
   //   rollback
-  // call
+
+  /**
+   * Calls stored procedure
+   *
+   * @tparam H completion handler
+   * @tparam Args procedure arguments type
+   * @param function procedure name
+   * @param args procedure arguments (MP_ARRAY)
+   * @param handler completion handler
+   * @returns CallResult - a struct that holds anything as a result or an error
+   *
+   * Args must be vector of arguments or a tuple. Argument list will be destructured into the named
+   * arguments of the function.
+   *
+   * Example:
+   * function foo(a) ...
+   * call("foo", std::make_tuple(5));
+   *
+   * It may be more convenient to create a struct and specialize serialization function for it.
+   * Look msgpack-cxx for information.
+   */
+  template<class H, class Args>
+  auto call(std::string_view function, Args&& args, H&& handler)
+  {
+    detail::RequestPacker packer;
+    detail::iproto::MessageHeader header;
+    header.sync = m_state->m_conn->generate_id();
+    header.request_type = detail::iproto::RequestType::Call;
+    packer.pack(header);
+    packer.pack(function);
+    packer.pack(std::forward<decltype(args)>(args));
+    packer.finalize();
+
+    return boost::asio::async_initiate<H, void(error_code, GenericResult)>(
+        boost::asio::experimental::co_composed<H, void(error_code, GenericResult)>(
+            [this](
+                auto state, detail::iproto::OperationId id, detail::RequestPacker buffer) -> void
+            {
+              auto [ec, buf] = co_await m_state->m_conn->send_request(
+                  detail::Operation {
+                      .id = id, .data = detail::Data(buffer.str().data(), buffer.str().size())},
+                  boost::asio::as_tuple(boost::asio::deferred));
+
+              co_return state.complete(ec, GenericResult(buf));
+            }),
+        handler,
+        header.sync,
+        std::move(packer));
+  }
+
+  /**
+   * Execute lua code
+   *
+   * @tparam H completion handler type
+   * @tparam Args list of arguments (MP_ARRAY)
+   * @param expression lua code
+   * @param args arguments
+   * @param handler completion handler
+   */
+  template<class H, class Args>
+  auto eval(std::string_view expression, Args&& args, H&& handler)
+  {
+    detail::RequestPacker packer;
+    detail::iproto::MessageHeader header;
+    header.sync = m_state->m_conn->generate_id();
+    header.request_type = detail::iproto::RequestType::Eval;
+    packer.pack(header);
+    packer.pack(expression);
+    packer.pack(std::forward<decltype(args)>(args));
+    packer.finalize();
+
+    return boost::asio::async_initiate<H, void(error_code, GenericResult)>(
+        boost::asio::experimental::co_composed<H, void(error_code, GenericResult)>(
+            [this](
+                auto state, detail::iproto::OperationId id, detail::RequestPacker buffer) -> void
+            {
+              auto [ec, buf] = co_await m_state->m_conn->send_request(
+                  detail::Operation {
+                      .id = id, .data = detail::Data(buffer.str().data(), buffer.str().size())},
+                  boost::asio::as_tuple(boost::asio::deferred));
+
+              co_return state.complete(ec, GenericResult(buf));
+            }),
+        handler,
+        header.sync,
+        std::move(packer));
+  }
+
   // sql
-  // eval
 
   /**
    * Allows to get the underlying connection. May be useful to create another
@@ -50,6 +223,6 @@ private:
   std::shared_ptr<BoxInternal> m_state;
 };
 
-}
+}  // namespace tntpp::box
 
 #endif  // TARANTOOL_CONNECTOR_TNTPP_BOX_H
